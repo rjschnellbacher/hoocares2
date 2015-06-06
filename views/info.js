767649c0b3f0bef7967b1b6d4f677ce2b13ff050
Michael Goldman <goldmandrummer@gmail.com>
2015-06-06 16:55:45 -0400

    grab input fields and create visitor

---
+++ views/info.js
@@ -0,0 +1,15 @@
+function submitInfo() {
+	var obj = {};
+	obj["first_name"] = $(#first_name).value;
+	obj["last_name"] = $(#last_name).value;
+	obj["addr1"] = $(#address_1).value;
+	obj["addr2"] = $(#address_2).value;
+	obj["city"] = $(#city).value;
+	obj["state"] = $(#state).value;
+	obj["zip"] = $(#zip).value;
+	obj["ssn"] = $(#ssn).value;
+	obj["dob"] = $(#dob).value;
+	obj["addr2"] = $(#address_2).value;
+
+	Visitors.create(obj);
+}
