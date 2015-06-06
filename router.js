16202654107dfa5e95ab631adb70b48f801503ef
Michael Goldman <goldmandrummer@gmail.com>
2015-06-06 14:24:59 -0400

    added router and got 2 views working

---
+++ router.js
@@ -0,0 +1,24 @@
+Router.route('/', function () {
+  this.render('signin');
+});
+
+Router.route('/employee', function () {
+  this.render('employee');
+});
+
+// Router.route('/items/:_id', function () {
+//   var item = Items.findOne({_id: this.params._id});
+//   this.render('ShowItem', {data: item});
+// });
+
+// Router.route('/files/:filename', function () {
+//   this.response.end('hi from the server\n');
+// }, {where: 'server'});
+
+// Router.route('/restful', {where: 'server'})
+//   .get(function () {
+//     this.response.end('get request\n');
+//   })
+//   .post(function () {
+//     this.response.end('post request\n');
+//   });
